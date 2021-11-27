1eebd1b14896cd8c93edf0f163c8e2954e994c95
evoked <3vok3d@gmail.com>
2021-11-27 18:17:59 +0000

    start: client services

---
+++ client/src/services/order.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const getOrder = (order) => {
+
+}
+
+const deleteOrder = (order) => {
+
+}
+
+const newOrder = (order) => {
+
+}
+
+const getAllOrder = () => {
+
+}
+
+module.exports = {
+    getOrder, deleteOrder, newOrder, getAllOrder
+}
